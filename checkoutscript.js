3b9786f0e2b517ed7b200c4bdf7cf3015223858d
carolinapelloni <162307499+carolinapelloni@users.noreply.github.com>
2024-07-06 14:45:07 +0200

    Update checkoutscript.js

--- checkoutscript.js
+++ checkoutscript.js
@@ -3,36 +3,10 @@
     // Get a reference to the button
     var checkoutButton = document.getElementById("checkout-button");
 
-    // Add an onclick event listener to the button
-    checkoutButton.addEventListener("click", function() {
-        // Parse query parameters
-        const queryParams = new URLSearchParams(window.location.search);
-
-        // Get the values and set them to the corresponding hidden input fields
-        document.getElementById("totalAmountInput").value = queryParams.get("totalAmount");
-        document.getElementById("id").value = queryParams.get("id");
-        document.getElementById("shippingCostInput").value = queryParams.get("shippingCost");
-        document.getElementById("couponCodeInput").value = queryParams.get("couponCode");
-        document.getElementById("customerEmailInput").value = queryParams.get("customerEmail");
-    });
-
     // Automatically click the checkout button when the page loads
     document.addEventListener("DOMContentLoaded", function() {
         var checkoutButton = document.getElementById("checkout-button");
         checkoutButton.click();
     });
 
-    // Second script block
-    // Parse query parameters
-    const queryParams = new URLSearchParams(window.location.search);
-
-    // Get the totalAmount value and set it to the corresponding HTML element
-    const totalAmount = queryParams.get("totalAmount");
-    document.getElementById("totalAmountDisplay").value = totalAmount;
-
-    // Automatically click the checkout button when the page loads
-    document.addEventListener("DOMContentLoaded", function() {
-        var checkoutButton = document.getElementById("checkout-button");
-        checkoutButton.click();
-    });
 }();
